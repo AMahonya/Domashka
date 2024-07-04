7e6460bde2e542cf4b65953f8dc6da33cbd593d2
AMahonya <mahanec.andrei@gmail.com>
2024-07-04 21:29:00 +0300

    ВЕРСИЯ 4 Вроде понял разницу между атрибутами объекта и класса, дополнив уже существующий класс применив метод __new__. применил знания о перегурзке арифметических операторов и операторов сравнения. применил на практике знания о классах, объектах и их атрибтах + практика по базовым магическим методам .
    1. Создал класс House.
    2. Вунтри класса House определил метод __init__, в который передал название и кол-во этажей.
    3. Внутри метода __init__ создал атрибуты объекта self.name и self.number_of_floors, присвоил им переданные значения.
    4. Создал метод go_to с параметром new_floor и написал логику внутри него на основе описания задачи.
    5. Создал объект класса House с названием и количеством этажей.
    6. Дополнил класс House двумя методами __len__ и __str__ где __len__ - возвращает кол-во этажей здания, а __str__ возвращает строку название и кол-во этажей.
    7. Добавил методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=), __eq__(==) которые возвращают результат сравнения каждый по соответствующему оператору
    8. Добавил метод __add__ который увеличивает кол-во этажей на переданные значения value
    9. Методы  __radd__ и __iadd__ возвращают значение вызова __add__.
    10. Применил метод __new__ в котором получаю название дома из args по индексу 0 потому что первый обект будет название дома, далее добавил название дома в список cls.houses_history, используя ссылку на сам класс. далее использовал super класс для создания нового обьекта
    11. переопределил метод __del__ который выводит строку "<название> снесён, но он останется в истории"

--- module 5/module 5 1.py
+++ module 5/module 5 1.py
@@ -1,4 +1,10 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        houses_name = args[0]  # получаем название дома из args по индексу
+        cls.houses_history.append(houses_name)  # добавляем название дома в атрибут houses_history
+        return super().__new__(cls)  # создаем новый обьект
 
     def __init__(self, name, number_of_floors):
         self.name = name
@@ -22,8 +28,6 @@ class House:
     def __eq__(self, other):
         isinstance(other, int)
         return self.number_of_floors == other.number_of_floors
-
-
 
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
@@ -53,29 +57,62 @@ class House:
     def __iadd__(self, value):
         return self.__add__(value)
 
-
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2)  # __eq__
+print(House.houses_history)
 
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
 
-h1 += 10  # __iadd__
-print(h1)
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
 
-h2 = 10 + h2  # __radd__
-print(h2)
 
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# # __str__
+# print(h1)
+# print(h2)
+#
+# # __len__
+# print(len(h1))
+# print(len(h2))
+
+
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(h1 == h2)  # __eq__
+#
+# h1 = h1 + 10  # __add__
+# print(h1)
+# print(h1 == h2)
+#
+# h1 += 10  # __iadd__
+# print(h1)
+#
+# h2 = 10 + h2  # __radd__
+# print(h2)
+#
+# print(h1 > h2)  # __gt__
+# print(h1 >= h2)  # __ge__
+# print(h1 < h2)  # __lt__
+# print(h1 <= h2)  # __le__
+# print(h1 != h2)  # __ne__
