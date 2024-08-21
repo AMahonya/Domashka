3e9bbbdfd7497b4715ac61c60ef2f77136a4d5ba
AMahonya <mahanec.andrei@gmail.com>
2024-08-21 18:58:48 +0300

    Научился создавать классы наследованные от класса Thread решив задачу.V2
    Создал класс Knight который наследует от класса Thread.
    Создал и запустите 2 потока на основе класса Knight.
    Вывел на экран строку об окончании битв.

--- module 10/module 10 2.py
+++ module 10/module 10 2.py
@@ -14,17 +14,21 @@ class Knight(Thread):
         print(f"{self.name} На нас напали стражи")
         day_counter = 0
         while self.max_power > 0:
-            self.max_power -= self.power
-            day_counter += 1
+            if self.max_power >= self.power:
+                self.max_power -= self.power
+            else:
+                self.power = self.max_power
+                self.max_power = 0
 
+            day_counter += 1
             print(f"{self.name} сражается {day_counter}-е сутки, осталось {self.max_power} стражей ")
             time.sleep(1)
         print(f"{self.name} одержал победу спустя: {day_counter}-е сутки")
 
 
 if __name__ == "__main__":
-    first_knight = Knight('Sir Lancelot', 10)
-    second_knight = Knight("Sir Galahad", 20)
+    first_knight = Knight('Sir Lancelot', 48)
+    second_knight = Knight("Sir Galahad", 9)
 
     first_knight.start()
     second_knight.start()
