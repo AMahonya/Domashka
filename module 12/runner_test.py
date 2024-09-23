6efabf306c11c89e2acbc3c3c077c6d495468155
AMahonya <mahanec.andrei@gmail.com>
2024-09-23 20:08:08 +0300

    Добавил строги для логирования

--- module 12/runner_test.py
+++ module 12/runner_test.py
@@ -1,3 +1,6 @@
+import logging
+import traceback
+
 from runner import Runner
 import unittest
 
@@ -7,18 +10,26 @@ class TestRunner(unittest.TestCase):
 
     @unittest.skipIf(is_frothen, "Тесты в этом кейсе заморожены")
     def test_walk(self):
-        runner = Runner("John Doe")
-        for _ in range(10):
-            runner.walk()
-        self.assertEqual(runner.distance, 50)
-
-
+        try:
+            runner = Runner("John Doe", -5)
+            for _ in range(10):
+                runner.walk()
+            self.assertEqual(runner.distance, 50)
+            logging.info("'test_walk' выполнен успешно")
+        except ValueError as e:
+            logging.warning(f"Неверная скорость для Runner\n{e}")
+            logging.warning(traceback.format_exc())
 
     def test_run(self):
-        runner = Runner("Bob")
-        for _ in range(10):
-            runner.run()
-        self.assertEqual(runner.distance, 100)
+        try:
+            runner = Runner(113, 10)
+            for _ in range(10):
+                runner.run()
+            self.assertEqual(runner.distance, 100)
+            logging.info("'test_run' выполнен успешно")
+        except TypeError as e:
+            logging.warning(f"Неверный тип данных для обьекта Runner\n{e}")
+            logging.warning(traceback.format_exc())
 
     def test_challenge(self):
         runner1 = Runner("Alice")
@@ -28,6 +39,7 @@ class TestRunner(unittest.TestCase):
             runner2.walk()
 
         self.assertNotEqual(runner1.distance, runner2.distance)
+        logging.info("'test_challenge' выполнен успешно")
 
 
 if __name__ == "__main__":
